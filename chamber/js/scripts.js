4d597f632977232d13ab2f7ad241b1cc1f12386d
Samad <samad.damas@hotmail.com>
2022-10-14 13:19:22 -0300

    Add content to home page

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -28,6 +28,22 @@ const dayName = getDayName(date.getDay())
 const month = getMonthName(date.getMonth())
 const year = date.getFullYear()
 
+//Add banner to header
+function displayBanner(dayName) {
+
+    //Use this line to test the function: || dayName === "Current day name to test"
+    if (dayName === "Monday" || dayName === "Tuesday") {
+        const banner = document.querySelector("#banner")
+        banner.className = "display"
+        banner.innerHTML = "<p>🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.</p>"
+    } else {
+        const banner = document.querySelector("#banner")
+        banner.classList.toggle("display")
+    }
+}
+
+displayBanner(dayName)
+
 //Add current date to header
 document.querySelector("#currentDate").textContent = `${dayName}, ${dayNumber} ${month} ${year}`
 
